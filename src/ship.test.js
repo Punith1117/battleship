5d624f8acc3aa1d80f92e8b35d92d098dfc2caf6
Punith1117 <punith11172006@gmail.com>
2025-01-24 13:25:21 +0530

    add test to check if hitCount is a field of Ship class

--- src/ship.test.js
+++ src/ship.test.js
@@ -10,3 +10,8 @@ test('object constructor takes length as single parameter', () => {
     s = new Ship(3)
     expect(s).toHaveProperty('len', 3)
 })
+
+test('has property that counts number of hits taken', () => {
+    s = new Ship(3)
+    expect(s).toHaveProperty('hitCount', 0)
+})
