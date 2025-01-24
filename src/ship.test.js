a5ca68b53d79847576acc96656dcdacd0293b16b
Punith1117 <punith11172006@gmail.com>
2025-01-24 13:13:33 +0530

    add test to check if constructor initializes length

--- src/ship.test.js
+++ src/ship.test.js
@@ -5,3 +5,8 @@ let s = new Ship()
 test('class/object exists', () => {
     expect(s).toBeDefined();
 })
+
+test('object constructor takes length as single parameter', () => {
+    s = new Ship(3)
+    expect(s).toHaveProperty('len', 3)
+})
