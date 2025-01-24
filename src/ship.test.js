6f6bb8ff10b7e577545cf6bb72ec60a0babefc9b
Punith1117 <punith11172006@gmail.com>
2025-01-24 13:56:52 +0530

    modify to avoid initializing ship object again and again

--- src/ship.test.js
+++ src/ship.test.js
@@ -1,28 +1,24 @@
 import { Ship } from "./ship";
 
-let s = new Ship()
+let s = new Ship(3)
 
 test('class/object exists', () => {
     expect(s).toBeDefined();
 })
 
 test('object constructor takes length as single parameter', () => {
-    s = new Ship(3)
     expect(s).toHaveProperty('len', 3)
 })
 
 test('has property that counts number of hits taken', () => {
-    s = new Ship(3)
     expect(s).toHaveProperty('hitCount', 0)
 })
 
 test('has method to increase hitCount', () => {
-    s = new Ship(3)
     s.hit()
     expect(s).toHaveProperty('hitCount', 1)
 })
 
 test('has method to check if the ship has sunk', () => {
-    s = new Ship(3)
     expect(s.isSunk()).toBeFalsy()
 })
