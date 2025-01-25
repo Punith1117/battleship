4be54a686030d6d7d1b52cce643fb1d8a2201d29
Punith1117 <punith11172006@gmail.com>
2025-01-25 16:10:27 +0530

    add gameboard and cell class
    gameboard has placeShip function that returns false when index of array is invalid

---
+++ src/game-board.js
@@ -0,0 +1,17 @@
+import { Cell } from "./cell";
+import { Ship } from "./ship";
+
+export class GameBoard {
+    constructor() {
+        this.board = Array.from({ length: 8 }, () => Array.from({ length: 8 }, () => new Cell()));
+        this.ship5 = new Ship(5)
+        this.ship4 = new Ship(4)
+        this.ship3 = new Ship(3)
+        this.ship2 = new Ship(2)
+        this.ship1 = new Ship(1)
+    }
+
+    placeShip(x, y, shipLen, direction) {
+        if (x > 7 || y > 7 || x < 0 || y < 0) return false
+    }
+}
