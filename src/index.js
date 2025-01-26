45411b9b1a7e567badcc3d0450865831ce3e49df
Punith1117 <punith11172006@gmail.com>
2025-01-26 16:29:08 +0530

    add function to generate random position for ship

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-import "./reset.css";
-import "./style.css";
+export let getRandomPosition = () => {
+    let arr = []
+    let row = Math.round(Math.random() * 7)
+    let col = Math.round(Math.random() * 7)
+    arr.push(row)
+    arr.push(col)
+    return arr
+}
