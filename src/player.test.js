782329326df44be4dfef2154bcc56434bef980b7
Punith1117 <punith11172006@gmail.com>
2025-01-26 17:01:03 +0530

    add test to check if the player is assigned hasWon to be true
    it should happen when opponent's base is destroyed

--- src/player.test.js
+++ src/player.test.js
@@ -1,3 +1,4 @@
+import { getRandomPosition } from ".";
 import { Player } from "./player";
 
 let player = new Player()
@@ -11,5 +12,14 @@ describe('attack function', () => {
         let opBoard = opponent.board
         let cell = opBoard.board[0][0]
         expect(cell.value).toBe(0)
+    })
+
+    test('sets hasWon to be true if all ships of opponent are destroyed', () => {
+        while(player.hasWon == true) {
+            let position = getRandomPosition()
+            player.attack(position[0], position[1], opponent)
+        }
+        let opBoard = opponent.board
+        expect(opBoard.isDestroyed()).toBe(true)
     })
 })
