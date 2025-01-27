5e11c87265398e00b6cc31e143ef001a6fde8561
Punith1117 <punith11172006@gmail.com>
2025-01-27 14:24:28 +0530

    modify test to use spyOn method of jest

--- src/computer.test.js
+++ src/computer.test.js
@@ -1,9 +1,11 @@
 import { Computer } from "./computer";
 import { Player } from "./player";
 
-let computer = new Computer()
+let computer
 let opponent = new Player()
 
 test('ships should be randomly placed when the object is created', () => {
-    expect(computer.placeShipsRandomly()).toBe(false)//it should return false when it is called again
+    let spy = jest.spyOn(Computer.prototype, 'placeShipsRandomly')
+    computer = new Computer()
+    expect(spy).toBeCalled()//it should return false when it is called again
 })
