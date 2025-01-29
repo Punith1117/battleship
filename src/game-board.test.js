2f0d8f7292b1054ca9bf075b8980acec2749b55b
Punith1117 <punith11172006@gmail.com>
2025-01-29 13:41:29 +0530

    modify to test canPlaceShip function instead of placeShip function

--- src/game-board.test.js
+++ src/game-board.test.js
@@ -2,30 +2,32 @@ import { GameBoard } from "./game-board";
 
 let board = new GameBoard()
 
-describe('placeShip function', () => {
-    test('to return false if any coordinate is invalid', () => {
-        expect(board.placeShip(-1, 3, 5, 'h')).toBeFalsy()
+describe('canPlaceShip function', () => {
+    test('should return false if any coordinate is invalid', () => {
+        expect(board.canPlaceShip(-1, 3, 5, 'h')).toBeFalsy()
     })
-
+    
     test('should return false if ship length is invalid', () => {
-        expect(board.placeShip(1, 3, 6, 'h')).toBeFalsy()
+        expect(board.canPlaceShip(1, 3, 6, 'h')).toBeFalsy()
     })
-
+    
     test('should return false if the ship is already placed', () => {
         board.placeShip(1, 3, 4, 'h')
-        expect(board.placeShip(1, 3, 4, 'h')).toBeFalsy()
+        expect(board.canPlaceShip(1, 3, 4, 'h')).toBeFalsy()
     })
-
+    
     test('should return false if the ship size is too large to place in the coordinates given', () => {
-        expect(board.placeShip(0, 7, 2, 'h')).toBeFalsy()
-    })
-
-    test('should return true if a ship is placed successfully', () => {
-        expect(board.placeShip(0, 5, 2, 'h')).toBeTruthy()
+        expect(board.canPlaceShip(0, 7, 2, 'h')).toBeFalsy()
     })
 
     test('should return false if any cell in which the ship is placed is already occupied', () => {
-        expect(board.placeShip(1, 3, 3, 'h')).toBeFalsy()
+        expect(board.canPlaceShip(1, 3, 3, 'h')).toBeFalsy()
+    })
+})
+
+describe('placeShip function', () => {
+    test('should return true if a ship is placed successfully', () => {
+        expect(board.placeShip(0, 5, 2, 'h')).toBeTruthy()
     })
 })
 
