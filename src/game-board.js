4b2ad9084357c57ec712bb74ae0297f498f0a0d9
Punith1117 <punith11172006@gmail.com>
2025-01-30 13:45:09 +0530

    add another boat of length 2

--- src/game-board.js
+++ src/game-board.js
@@ -10,6 +10,7 @@ export class GameBoard {
         this.ships.push(new Ship(1))
         this.ships.push(new Ship(2))
         this.ships.push(new Ship(2))
+        this.ships.push(new Ship(2))
         this.ships.push(new Ship(3))
         this.ships.push(new Ship(3))
     }
@@ -66,7 +67,7 @@ export class GameBoard {
     placeShipsRandomly() {
         if (this.ships[0].isPlaced) return false
         let shipPlaced = false
-        let shipLenArr = [1, 1, 2, 2, 3, 3]
+        let shipLenArr = [1, 1, 2, 2, 2, 3, 3]
         for (let shipLen of shipLenArr) {
             shipPlaced = false
             while(shipPlaced == false) {
