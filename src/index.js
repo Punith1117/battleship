b9ae7bd4a231c834dfc8b5a2f8c1f5a206262bb8
Punith1117 <punith11172006@gmail.com>
2025-01-31 17:06:14 +0530

    remove unnecessary comment and console.log

--- src/index.js
+++ src/index.js
@@ -29,7 +29,6 @@ dispOpponentTurn.textContent = ''
 
 export async function handlePlayerClick(row, col) {
     if (playerTurn != 'player') return
-    console.log('clicked on ' + row + ' ' + col)
     player.attack(row, col, opponent)
     displayOpponentBoard(opponent)
     if (player.hasWon) {
@@ -79,79 +78,3 @@ function removeEventListeners() {
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
-/*
-let body = document.querySelector('body')
-    let board = document.createElement('div')
-    board.classList.add('opponent-board')
-    body.appendChild(board)
-
-    let playerBoard = document.createElement('div')
-    playerBoard.classList.add('player-board')
-    body.appendChild(playerBoard)
-
-function displayOpponentBoard() {
-    let body = document.querySelector('body')
-        let board = document.querySelector('.opponent-board')
-        board.innerHTML = ''
-        let boardArray = computer.board.boardArr
-        for (let row in boardArray) {
-            for (let col in boardArray) {
-                let cell = boardArray[row][col]
-                let dispCell = document.createElement('div')
-                dispCell.classList.add('cell')
-                dispCell.setAttribute('data-row', row)
-                dispCell.setAttribute('data-col', col)
-                if (cell.value == null) {
-                    dispCell.classList.add('no-value')
-                    dispCell.addEventListener('click', () => {
-                        console.log('clicked on ' + dispCell.getAttribute('data-row') + " " +  dispCell.getAttribute('data-col'))
-                        player.attack(row, col, computer)
-                        displayOpponentBoard()
-                        if (player.hasWon) {
-                            console.log('hurrah! You have won')
-                            return
-                        }
-                        computer.attack(player)
-                        if (computer.hasWon) {
-                            console.log(':( Computer has won')
-                            return
-                        }
-                        displayPlayerBoard()
-                        return
-                    })
-                } else if (cell.value == 0 && cell.isShip) {
-                    dispCell.classList.add('hit')
-                } else {
-                    dispCell.classList.add('miss')
-                }
-                board.appendChild(dispCell)
-            }
-        }
-        body.appendChild(board)
-}
-
-displayPlayerBoard()
-displayOpponentBoard()
-
-function displayPlayerBoard() {
-    let board = document.querySelector('.player-board')
-    board.innerHTML = ''
-    let boardArray = player.board.boardArr
-    for (let row in boardArray) {
-        for (let col in boardArray) {
-            let cell = boardArray[row][col]
-
-            let dispCell = document.createElement('div')
-            dispCell.classList.add('cell')
-            if (cell.isShip && cell.value == null) {
-                dispCell.classList.add('ship')
-            } else if (cell.value == 0 && cell.isShip) {
-                dispCell.classList.add('hit')
-            } else if (cell.value == 0){
-                dispCell.classList.add('miss')
-            }
-            board.appendChild(dispCell)
-        }
-    }
-}
-*/
